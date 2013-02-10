d1e069d77229626a9b1b4d153364393d6b53c5ea
Pooria Azimi <pooriaazimi@me.com>
2013-02-10 17:51:55 +0330

    a few css tricks and an Ad class

--- adserver.rb
+++ adserver.rb
@@ -1,7 +1,56 @@
 require 'sinatra/base'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-migrations'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/db/adserver.db")
+
+class Ad
+
+  include DataMapper::Resource
+
+  property :id,           Serial
+  property :title,        String
+  property :content,      Text
+  property :width,        Integer
+  property :height,       Integer
+  property :filename,     String
+  property :url,          String
+  property :is_active,    Boolean
+  property :created_at,   DateTime
+  property :updated_at,   DateTime
+  property :size,         Integer
+  property :content_type, String
+
+end
 
 class AdServer < Sinatra::Base
+
+  DataMapper::auto_upgrade! 
+
   get '/' do
-    'hello'
+    haml "hello"
   end
+  
+  get '/ad' do
+  end
+
+  get '/list' do
+  end
+
+  get '/new' do
+  end
+
+  post '/create' do
+  end
+
+  get '/delete/:id' do
+  end
+
+  get '/show/:id' do
+  end
+
+  get '/click/:id' do
+  end
+
 end
